ca95249529184ef731ea462b3bc3824a24ef45e6
Malberee <82259650+Malberee@users.noreply.github.com>
2023-12-05 15:49:30 +0200

    Update useRadioGroupContext.ts

--- src/components/RadioGroup/hooks/useRadioGroupContext.ts
+++ src/components/RadioGroup/hooks/useRadioGroupContext.ts
@@ -1,7 +1,7 @@
 import { createContext, useContext } from 'react'
 import { RadioGroupContextProps } from '../RadioGroup.types'
 
-export const RadioGroupContext = createContext<RadioGroupContextProps>({})
+export const RadioGroupContext = createContext<RadioGroupContextProps>({selectRadio: () => ''})
 
 export const useRadioGroupContext = () => {
   return useContext<RadioGroupContextProps>(RadioGroupContext)
